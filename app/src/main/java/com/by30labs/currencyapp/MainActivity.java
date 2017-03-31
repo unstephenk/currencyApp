40f04ddeba3125ae52d78e0d15cd18060eab4919
unstephenk <unstephenk@gmail.com>
2017-03-30 23:49:26 -0500

    created a button to display the name that was entered

--- app/src/main/java/com/by30labs/currencyapp/MainActivity.java
+++ app/src/main/java/com/by30labs/currencyapp/MainActivity.java
@@ -4,10 +4,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     public void clickFunction(View view){
+        EditText enterName = (EditText) findViewById(R.id.enterName);
+
+        Log.i("Info", enterName.getText().toString());
         Log.i("Info", "Button Pressed");
     }
 
