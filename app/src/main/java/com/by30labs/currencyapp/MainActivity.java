962cc8c0dbf06963c8380da79354e605bf94c381
unstephenk <unstephenk@gmail.com>
2017-03-31 19:11:07 -0500

    added code to toggle between two images

--- app/src/main/java/com/by30labs/currencyapp/MainActivity.java
+++ app/src/main/java/com/by30labs/currencyapp/MainActivity.java
@@ -1,10 +1,12 @@
 package com.by30labs.currencyapp;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,6 +17,23 @@ public class MainActivity extends AppCompatActivity {
 
         Log.i("Info", enterName.getText().toString());
         Log.i("Info", "Button Pressed");
+    }
+
+    public void changeImage (View view){
+        ImageView currentImage = (ImageView) findViewById(R.id.image);
+        String currentImageName = String.valueOf(currentImage.getTag());
+        Toast.makeText(MainActivity.this, currentImageName, Toast.LENGTH_SHORT).show();
+
+        if (currentImageName.equals("house")){
+            // switch to flask
+            currentImage.setTag("flask");
+            currentImage.setImageResource(R.drawable.flask);
+        }
+        else if (currentImageName.equals("flask")){
+            // switch to house
+            currentImage.setTag("house");
+            currentImage.setImageResource(R.drawable.house);
+        }
     }
 
     @Override
